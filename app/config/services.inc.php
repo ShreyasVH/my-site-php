e199a246fd87c0db8663413d7d56b9da63179ac9
Shreyas <shreyas.hande@quikr.com>
2018-10-20 12:24:25 +0530

    temp comment

--- app/config/services.inc.php
+++ app/config/services.inc.php
@@ -13,8 +13,8 @@ use app\helpers\AssetHelper;
 use Phalcon\Logger\Adapter\File as FileAdapter;
 use Phalcon\Session\Adapter\Files as Session;
 
-$dotenv = new Dotenv\Dotenv(APP_PATH);
-$dotenv->load();
+//$dotenv = new Dotenv\Dotenv(APP_PATH);
+//$dotenv->load();
 
 $di->set('view', function() {
     $view = new View();
