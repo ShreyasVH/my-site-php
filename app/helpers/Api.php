1a14b88b2c3d476d38f5ba0eadc1c7bc531d0d9c
Shreyas <shreyas.hande@quikr.com>
2018-12-11 18:16:30 +0530

    corrected if statement

--- app/helpers/Api.php
+++ app/helpers/Api.php
@@ -67,7 +67,9 @@ class Api extends BaseHelper
     {
         $url = $this->_getEndpoint() . $url;
         if(CommonUtils::isDebugMode())
-        // $this->logger->debug('Calling GET API. URL : ' . $url);
+        {
+            // $this->logger->debug('Calling GET API. URL : ' . $url);
+        }
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url );
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, 30000);
