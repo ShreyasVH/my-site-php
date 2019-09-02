c0055b79be375f32fbced75acf94622ac62fbd7c
Shreyas <shreyas.hande@gmail.com>
2019-09-03 00:09:14 +0530

    toggle fields based on cardType

--- public/js/cards.js
+++ public/js/cards.js
@@ -6,9 +6,24 @@ $(document).ready(function() {
         var cardTypeElement = $(this).find('option[value="' + cardTypeId + '"]')[0];
 
         var cardType = $(cardTypeElement).text().trim();
-        console.log(cardType);
-
-        // if()
+        cardActions.toggleAdditionalFields(cardType);
     });
-
 });
+
+var cardActions = {
+    toggleAdditionalFields: function(cardType) {
+        if ('MONSTER' === cardType) {
+            $('input[name="level"]').closest('.form-field').show();
+            $('select[name="attribute"]').closest('.form-field').show();
+            $('select[name="type"]').closest('.form-field').show();
+            $('input[name="attack"]').closest('.form-field').show();
+            $('input[name="defense"]').closest('.form-field').show();
+        } else {
+            $('input[name="level"]').closest('.form-field').hide();
+            $('select[name="attribute"]').closest('.form-field').hide();
+            $('select[name="type"]').closest('.form-field').hide();
+            $('input[name="attack"]').closest('.form-field').hide();
+            $('input[name="defense"]').closest('.form-field').hide();
+        }
+    }
+};
