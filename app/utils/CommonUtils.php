55614b1ffbb8595260dc4bdddac5bc0fc215215b
Shreyas <shreyas.hande@gmail.com>
2019-09-30 12:45:37 +0530

    stylinh changes

--- app/utils/CommonUtils.php
+++ app/utils/CommonUtils.php
@@ -13,6 +13,46 @@ use Phalcon\Mvc\User\Component;
 
 class CommonUtils extends Component
 {
+    const SCREEN_SIZE_XS = 'xs';
+
+    const SCREEN_SIZE_SM = 'sm';
+
+    const SCREEN_SIZE_MD = 'md';
+
+    const SCREEN_SIZE_LG = 'lg';
+
+    const INDIVIDUAL_GUTTER_WIDTH = 30;
+
+    private static $_max_container_widths = [
+        self::SCREEN_SIZE_XS => 320,
+        self::SCREEN_SIZE_SM => 750,
+        self::SCREEN_SIZE_MD => 970,
+        self::SCREEN_SIZE_LG => 1170
+    ];
+
+    public static function getBestCardDimensions($screen_size, $no_of_cards, $container_percent = 100)
+    {
+        $max_container_width = self::getMaxContainerWidth($screen_size);
+        $max_available_width = (int) floor($container_percent * $max_container_width / 100);
+        $total_gutter_width = $no_of_cards * self::INDIVIDUAL_GUTTER_WIDTH;
+
+        $remaining_width = $max_available_width - $total_gutter_width;
+
+        $max_width_per_card = intdiv($remaining_width, $no_of_cards);
+
+        $card_width = ($max_width_per_card - ($max_width_per_card % 3));
+        $card_height = ($card_width * 4 / 3);
+        return [
+            'width' => $card_width,
+            'height' => $card_height
+        ];
+    }
+
+    public static function getMaxContainerWidth($screen_size)
+    {
+        return self::$_max_container_widths[$screen_size];
+    }
+
     public static function getCurrentMode()
     {
         $di = Di::getDefault();
