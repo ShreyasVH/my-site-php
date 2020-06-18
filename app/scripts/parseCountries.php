acb62171d0baef94917e22a12baec03e590e5345
shreyas <shreyas.hande@gmail.com>
2020-06-18 12:26:22 +0530

    added script for adding countries

---
+++ app/scripts/parseCountries.php
@@ -0,0 +1,42 @@
+<?php
+
+date_default_timezone_set('Asia/Kolkata');
+define('APP_PATH', realpath(dirname(dirname(dirname(__FILE__)))) . '/');
+
+$dataDirectory = APP_PATH . 'app/documents/cricbuzz/yearWiseDetails';
+
+$files = scandir($dataDirectory);
+$files = array_filter($files, function($file){
+    return (($file !== '.') && ($file !== '..'));
+});
+
+$countries = [];
+
+foreach($files as $file)
+{
+    $data = json_decode(file_get_contents($dataDirectory . '/' . $file), true);
+
+    foreach($data as $seriesName => $seriesDetails)
+    {
+        echo "\n" . $seriesName . "\n";
+        foreach($seriesDetails as $matchName => $matchDetails)
+        {
+            echo "\n\t" . $matchName . "\n";
+            foreach($matchDetails['players'] as $player)
+            {
+                if((array_key_exists('country', $player)) && !in_array($player['country'], $countries))
+                {
+                    $countries[] = $player['country'];
+                }
+            }
+
+            if(!is_null($matchDetails['stadium']['country']) && !in_array($matchDetails['stadium']['country'], $countries))
+            {
+                $countries[] = $matchDetails['stadium']['country'];
+            }
+        }
+    }
+}
+usort($countries, 'strcasecmp');
+
+file_put_contents(APP_PATH . 'app/documents/cricbuzz/countries.json', json_encode($countries, JSON_PRETTY_PRINT));
