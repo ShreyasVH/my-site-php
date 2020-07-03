3f0776162c02c7003739fb081af3103e1e5d8000
shreyas <shreyas.hande@gmail.com>
2020-07-03 18:00:02 +0530

    checking for tours before importing

--- app/scripts/importTours.php
+++ app/scripts/importTours.php
@@ -59,7 +59,35 @@ function addTour($payload)
     return $apiHelper->post('cricbuzz/tours', $payload, 'CRICBUZZ');
 }
 
+function getExistingTours()
+{
+    global $apiHelper;
+    $tours = [];
+
+    for($year = 1890; $year <= date('Y'); $year++)
+    {
+        $payload = [
+            'year' => $year,
+            'count' => 1000
+        ];
+
+        $apiResponse = $apiHelper->post('cricbuzz/tours/filter', $payload, 'CRICBUZZ');
+        if($apiResponse['status'] === 200)
+        {
+            $decodedResponse = json_decode($apiResponse['result'], true);
+            $tours = array_merge($tours, array_column($decodedResponse, 'name'));
+        }
+        else
+        {
+            echo "\nTour loading failed - " . $year . "\n";
+        }
+    }
+
+    return $tours;
+}
+
 $tours = getTours();
+$existingTours = getExistingTours();
 
 $index = 0;
 foreach($tours as $tour)
@@ -71,30 +99,37 @@ foreach($tours as $tour)
 
     echo "\nProcessing Tour. [" . ($index + 1) . "/" . count($tours) . "]\n";
 
-    $payload = [
-        'name' => $tour['name'],
-        'startTime' => date('Y-m-d H:i:s', $tour['startTime'] / 1000),
-        'endTime' => date('Y-m-d H:i:s', $tour['endTime'] / 1000)
-    ];
-
-    $response = addTour($payload);
-    if(200 === $response['status'])
+    if(in_array($tour['name'], $existingTours))
     {
         $stats['success']++;
     }
     else
     {
-        $stats['failure']++;
-        $failures[] = [
+        $payload = [
             'name' => $tour['name'],
-            'payload' => json_encode($payload),
-            'response' => $response['result'],
-            'status' => $response['status']
+            'startTime' => date('Y-m-d H:i:s', $tour['startTime'] / 1000),
+            'endTime' => date('Y-m-d H:i:s', $tour['endTime'] / 1000)
         ];
+
+        $response = addTour($payload);
+        if(200 === $response['status'])
+        {
+            $stats['success']++;
+        }
+        else
+        {
+            $stats['failure']++;
+            $failures[] = [
+                'name' => $tour['name'],
+                'payload' => json_encode($payload),
+                'response' => $response['result'],
+                'status' => $response['status']
+            ];
+        }
     }
 
-    writeData(APP_PATH . 'app/documents/importTourStats.txt', json_encode($stats, JSON_PRETTY_PRINT));
-    writeData(APP_PATH . 'app/documents/importTourFailures.txt', json_encode($failures, JSON_PRETTY_PRINT));
+    writeData(APP_PATH . 'logs/importTourStats.txt', json_encode($stats, JSON_PRETTY_PRINT));
+    writeData(APP_PATH . 'logs/importTourFailures.txt', json_encode($failures, JSON_PRETTY_PRINT));
 
     echo "\nProcessed Tour. [" . ($index + 1) . "/" . count($tours) . "]\n";
     $index++;
